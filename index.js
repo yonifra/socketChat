f464ea098fc03c74246fe205a97a6a19e95f2428
Jonathan Fraimorice <jonathanfr@wix.com>
2020-01-19 16:07:28 +0200

    emitting and receiving messages

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+    socket.broadcast.emit('hi 👋');
     console.log('a user connected');
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
     socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
+        io.emit('chat message', msg);
     });
 });
 
