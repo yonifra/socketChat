c306f60b5bb151f6684021385e022e2e90226b1d
Jonathan Fraimorice <jonathanfr@wix.com>
2020-01-19 16:02:48 +0200

    support messages

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ io.on('connection', function(socket){
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+    });
 });
 
 http.listen(port, function(){
