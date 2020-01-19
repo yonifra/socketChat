c8ff89c7ebaa409dd27ceba56527660a1d212d34
Jonathan Fraimorice <jonathanfr@wix.com>
2020-01-19 15:37:05 +0200

    initial addition of socket.io

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 const port = 3002;
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
 });
 
 http.listen(port, function(){
